842160833dd2799109567f1a229fa568f7b70acb
Ross Valler <ross@valler.ca>
2020-06-12 16:40:21 -0400

    Make local in-memory store threadsafe

--- providers/local/local.go
+++ providers/local/local.go
@@ -1,26 +1,37 @@
 package local
 
+import "sync"
+
 type Local struct {
 	store map[string][]byte
+	mu    sync.RWMutex
 }
 
 func New() Local {
 	return Local{store: map[string][]byte{}}
 }
 
-func (l Local) Get(key string) []byte {
+func (l *Local) Get(key string) []byte {
+	l.mu.RLock()
+	defer l.mu.RUnlock()
 	return l.store[key]
 }
 
-func (l Local) Put(key string, data []byte) error {
+func (l *Local) Put(key string, data []byte) error {
+	l.mu.Lock()
 	l.store[key] = data
+	l.mu.Unlock()
 	return nil
 }
 
-func (l Local) Delete(key string) {
+func (l *Local) Delete(key string) {
+	l.mu.Lock()
 	delete(l.store, key)
+	l.mu.Unlock()
 }
 
-func (l Local) Flush() {
+func (l *Local) Flush() {
+	l.mu.Lock()
 	l.store = map[string][]byte{}
+	l.mu.Unlock()
 }
